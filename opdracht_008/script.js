e3c4979c23a245e77642feabb8990d5b2756e7bf
Bibi <84073518+xBibie@users.noreply.github.com>
2021-06-26 13:29:36 +0200

    Add files via upload

---
+++ opdracht_008/script.js
@@ -0,0 +1,9 @@
+console.log("Hallo Winc Academy Studenten")
+// dit is een grote som
+/*
+let sum = 1230941 x 1823794 
+console.log("sum:", sum);
+let devide = 1637263 / 54;
+console.log("devide:", devide);
+*/
+// ik begrijp niks van
